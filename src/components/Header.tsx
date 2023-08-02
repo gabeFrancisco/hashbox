aab7cbd2a0793298bc329b3d3e326c6407598cf2
gabeFrancisco <gabegit2@gmail.com>
2023-08-01 22:58:27 -0300

    updated logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import React from "react";
 
+import logo from '../../public/logo.svg'
+import Image from "next/image";
+
 const Header = () => {
   const links = [
     {
@@ -18,7 +21,7 @@ const Header = () => {
   ];
   return (
     <header className="top-1 sticky shadow-lg bg-white flex flex-col md:flex-row lg:flex-row  lg:w-full items-center border-b-2  ">
-      <div className="text-3xl mx-10 text-gray-800 lg:text-md">🄷🄰🅂🄷<span className="text-blue-800">🄱🄾🅇</span></div>
+      <div className="mx-10 text-gray-800 lg:text-md w-52 flex flex-row items-center justify-center"><Image className="text-blue-800" src={logo} alt="Logo"/></div>
       <div>
         <ul className="flex flex-row text-gray-800 text-sm lg:text-md">
           {links.map((el, key) => (
