dad0729210c9945ba692e2092c755aad2651bb78
gabeFrancisco <gabegit2@gmail.com>
2023-08-05 17:19:08 -0300

    finished Information component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
   title: "hashBox - Hash Generator",
   description:
     "Generate hashes, GUIDs and much more! A complete dev-friend app!",
+  icons: ["favicon.ico"],
 };
 
 export default function RootLayout({
@@ -27,9 +28,16 @@ export default function RootLayout({
         crossOrigin="anonymous"
       />
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body className={inter.className}>
         <Header />
--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
+"use client";
 
-interface InformationProps{
-  title: string
-  text: string
+import React, { useState } from "react";
+
+interface InformationProps {
+  title: string;
+  text: string;
 }
 
 const Information = (props: InformationProps) => {
+  const [toggleView, setToggleView] = useState(false);
+  const handleView = () => {
+    toggleView ? setToggleView(false) : setToggleView(true);
+    console.log(toggleView);
+  };
   return (
-    <div className='my-5 py-2 px-3 border border-gray-200 shadow-lg rounded-lg'>
-      <div className='pt-1 border-b border-gray-200'>
-        <h3 className='text-xl'>{props.title}</h3>
+    <div className="my-5 py-2 px-3 border border-gray-200 rounded-lg">
+      <div className="py-1 border-b border-gray-200 flex flex-row justify-between">
+        <h3 className="text-xl">{props.title}</h3>
+        <button
+          onClick={handleView}
+          className="border rounded-lg px-3 py-1 bg-blue-800 hover:bg-blue-900 text-white"
+        >
+          {toggleView ? "↑" : "↓"}
+        </button>
       </div>
+      {toggleView ? (
+        <div className="py-3">
+          <p>{props.text}</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Information
+export default Information;
