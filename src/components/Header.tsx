caa85294ce04ff4922050153cdf74ddd60dd5b92
gabeFrancisco <gabegit2@gmail.com>
2024-08-31 23:28:30 -0300

    added robots.txt and sitemap | changed header height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-import logo from "../../public/logo.svg";
+import logo from "../../public/logo.webp"
 import Image from "next/image";
 
 const Header = () => {
@@ -24,14 +24,14 @@ const Header = () => {
     },
   ];
   return (
-    <header className="top-0 sticky bg-white shadow-md flex flex-col md:flex-row lg:flex-row  lg:w-full items-center z-30">
-      <div className="mx-10 mt-5 text-gray-800 md:mt-0 lg:text-md w-52 flex flex-row items-center justify-center">
+    <header className="sticky top-0 z-30 flex flex-col items-center bg-white shadow-md md:flex-row lg:flex-row lg:w-full">
+      <div className="flex flex-row items-center justify-center mx-10 mt-5 text-gray-800 md:mt-0 lg:text-md w-52">
         <Image className="text-blue-800" src={logo} alt="Logo" />
       </div>
       <div>
-        <ul className="flex flex-row text-gray-800 text-sm lg:text-md">
+        <ul className="flex flex-row text-sm text-gray-800 lg:text-md">
           {links.map((el, key) => (
-            <li className="m-5 hover:text-blue-800">
+            <li className="m-4 hover:text-blue-800">
               <Link href={el.link}>{el.title}</Link>
             </li>
           ))}
